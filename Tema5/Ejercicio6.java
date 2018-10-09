d700615aab003faed7346861565ede86403f462a
Anabel Castejón Martínez <ancastm@hotmail.com>
2018-10-09 10:24:02 +0200

    Actualizando Tema5

---
+++ Tema5/Ejercicio6.java
@@ -0,0 +1,19 @@
+/**Muestra los números del 320 al 160 contando de 20 en 20 hacia atrás, utilizando
+ * un bucle do-while
+ */
+ 
+ public class Ejercicio6 {
+   public static void main(String[] args) {
+    
+    int i = 320;
+    
+    do {
+      System.out.println(i);
+      i-=20;
+    }while(i >=160);
+    
+     
+     
+   }
+ }
+ 
