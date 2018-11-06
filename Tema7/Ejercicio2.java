8cf23e13fdaa075f436f30e8239a2c454028ea31
Anabel Castejón Martínez <ancastm@hotmail.com>
2018-11-06 09:20:21 +0100

    Actualizar Tema 7

---
+++ Tema7/Ejercicio2.java
@@ -0,0 +1,30 @@
+
+package ejercicio2;
+
+/**Define un array de 10 caracteres con nombre simbolo y asigna valores
+ * a los elementos según la tabla que se muestra a continuación. Muestra
+ * el contenido de todos los elementos del array. ¿Qué sucede con los valores 
+ * de los elementos que no han sido inicializados?
+ *
+ * @author Anabel Castejón Martínez
+ */
+public class Ejercicio2 {
+
+   public static void main(String[] args) {
+    
+     char[] simbolo = new char[10];
+     
+     simbolo [0] = 'a';
+     simbolo [1] = 'x';
+     simbolo [4] = '@';
+     simbolo [6] = ' ';
+     simbolo [7] = '+';
+     simbolo [8] = 'Q';
+     
+     for (int i = 0; i <10; i++) {
+       System.out.print(simbolo[i]);
+     }
+     
+  }
+  
+}
